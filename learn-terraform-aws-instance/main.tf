6ff8e84ad5fb98ac1609fda370dafe9ca206917c
Otto Quaglio <otto.q@gmx.com>
2022-10-05 06:56:26 +0800

    Add files already in progress

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,32 @@
+terraform {
+
+  cloud {
+    organization = "ottokorp"
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = "ami-0808460885ff81045"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-0309088508b70b6a0"]
+  subnet_id              = "subnet-025ec875d1fb1af29"
+  tags = {
+    Name = var.instance_name
+  }
+}
